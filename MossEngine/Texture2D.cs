6a1927656f407c9f0dc192f299321c3560da8fc4
Bubbies <66354328+bub-bl@users.noreply.github.com>
2025-12-03 21:54:57 +0100

    Comment this code for now, not used currently

--- MossEngine/Texture2D.cs
+++ MossEngine/Texture2D.cs
@@ -1,28 +1,28 @@
-using Silk.NET.WebGPU;
-
-namespace MossEngine;
-
-public sealed class Texture2D : IDisposable
-{
-	private unsafe Texture* _texture;
-	public TextureFormat Format { get; }
-	public uint Width { get; }
-	public uint Height { get; }
-	public uint MipLevels { get; }
-	public TextureUsage Usage { get; }
-	public TextureAspect Aspect { get; }
-
-	public static unsafe implicit operator Texture*( Texture2D texture )
-	{
-		return texture._texture;
-	}
-
-	public void Dispose()
-	{
-		unsafe
-		{
-			WebGpu.Wgpu.TextureDestroy( _texture );
-			WebGpu.Wgpu.TextureRelease( _texture );
-		}
-	}
-}
+// using Silk.NET.WebGPU;
+//
+// namespace MossEngine;
+//
+// public sealed class Texture2D : IDisposable
+// {
+// 	private unsafe Texture* _texture;
+// 	public TextureFormat Format { get; }
+// 	public uint Width { get; }
+// 	public uint Height { get; }
+// 	public uint MipLevels { get; }
+// 	public TextureUsage Usage { get; }
+// 	public TextureAspect Aspect { get; }
+//
+// 	public static unsafe implicit operator Texture*( Texture2D texture )
+// 	{
+// 		return texture._texture;
+// 	}
+//
+// 	public void Dispose()
+// 	{
+// 		unsafe
+// 		{
+// 			WebGpu.Wgpu.TextureDestroy( _texture );
+// 			WebGpu.Wgpu.TextureRelease( _texture );
+// 		}
+// 	}
+// }
