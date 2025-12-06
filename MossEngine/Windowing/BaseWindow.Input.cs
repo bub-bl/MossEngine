4663f964153032309d9cb24b8737a65f34290c6b
Bubbies <66354328+bub-bl@users.noreply.github.com>
2025-12-06 14:58:15 +0100

    RootPanel cannot be null. Thats remove warnings

--- MossEngine/Windowing/BaseWindow.Input.cs
+++ MossEngine/Windowing/BaseWindow.Input.cs
@@ -57,7 +57,7 @@ public abstract partial class BaseWindow
 	private void InternalOnMouseMove( IMouse arg1, Vector2 arg2 )
 	{
 		OnMouseMove( arg2 );
-		RootPanel?.ProcessPointerMove( arg2 );
+		RootPanel.ProcessPointerMove( arg2 );
 	}
 
 	private void InternalOnMouseUp( IMouse arg1, MouseButton arg2 )
@@ -65,7 +65,7 @@ public abstract partial class BaseWindow
 		var e = new MouseEventArgs { Button = arg2, Position = arg1.Position };
 
 		OnMouseUp( e );
-		RootPanel?.ProcessPointerUp( e );
+		RootPanel.ProcessPointerUp( e );
 	}
 
 	private void InternalOnMouseDown( IMouse arg1, MouseButton arg2 )
@@ -73,7 +73,7 @@ public abstract partial class BaseWindow
 		var e = new MouseEventArgs { Button = arg2, Position = arg1.Position };
 
 		OnMouseDown( e );
-		RootPanel?.ProcessPointerDown( e );
+		RootPanel.ProcessPointerDown( e );
 	}
 
 	private void InternalOnDoubleClick( IMouse arg1, MouseButton arg2, Vector2 arg3 )
@@ -86,13 +86,13 @@ public abstract partial class BaseWindow
 	private void InternalOnKeyUp( IKeyboard arg1, Key arg2, int arg3 )
 	{
 		OnKeyUp( arg2 );
-		RootPanel?.ProcessKeyUp( arg2 );
+		RootPanel.ProcessKeyUp( arg2 );
 	}
 
 	private void InternalOnKeyDown( IKeyboard arg1, Key arg2, int arg3 )
 	{
 		OnKeyDown( arg2 );
-		RootPanel?.ProcessKeyDown( arg2 );
+		RootPanel.ProcessKeyDown( arg2 );
 	}
 
 	protected virtual void OnKeyUp( Key key )
