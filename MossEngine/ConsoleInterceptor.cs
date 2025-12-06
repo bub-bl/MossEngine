91c4219dc164d828c6efbcb4258cb831a536b8d6
Bubbies <66354328+bub-bl@users.noreply.github.com>
2025-12-06 20:56:27 +0100

    Add temporary console interceptor to get console out

---
+++ MossEngine/ConsoleInterceptor.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace MossEngine;
+
+public sealed class ConsoleInterceptor( TextWriter original ) : TextWriter
+{
+	public static event Action<string>? OnWrite;
+
+	public override Encoding Encoding => original.Encoding;
+
+	public override void Write( string? value )
+	{
+		OnWrite?.Invoke( value ?? "" );
+		original.Write( value );
+	}
+
+	public override void WriteLine( string? value )
+	{
+		OnWrite?.Invoke( value ?? "" );
+		original.WriteLine( value );
+	}
+}
--- MossEngine/Program.cs
+++ MossEngine/Program.cs
@@ -1,6 +1,17 @@
 using MossEngine;
 
+var interceptor = new ConsoleInterceptor( Console.Out );
+
+ConsoleInterceptor.OnWrite += ( msg ) =>
+{
+	// Ici tu captures chaque log console
+	// Tu peux stocker, filtrer ou envoyer ailleurs.
+	Console.Error.WriteLine( $"[CAPTURE] {msg}" );
+};
+
+Console.SetOut( interceptor );
+
 WebGpu.Initialize();
 
-var window = new GameWindow();
+var window = new EditorWindow();
 window.Run();
