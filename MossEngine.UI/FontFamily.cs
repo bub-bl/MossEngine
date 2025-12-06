2deac92de6c54d08184ada167fd06d401d9b5c5c
Bubbies <66354328+bub-bl@users.noreply.github.com>
2025-12-06 20:55:10 +0100

    Push missing FontFamily struct for text panel

---
+++ MossEngine.UI/FontFamily.cs
@@ -0,0 +1,21 @@
+﻿using SkiaSharp;
+
+namespace MossEngine.UI;
+
+public readonly record struct FontFamily
+{
+	public readonly string Name;
+	public readonly SKTypeface Typeface;
+	
+	private FontFamily( string name, SKTypeface typeface )
+	{
+		Name = name;
+		Typeface = typeface;
+	}
+
+	public static FontFamily Load( string name, string path )
+	{
+		var typeface = SKTypeface.FromFile( path );
+		return new FontFamily( name, typeface );
+	}
+}
