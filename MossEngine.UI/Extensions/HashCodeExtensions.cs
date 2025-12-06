0ede104cf03e1507a396c140628a35d1a4c899ef
Bubbies <66354328+bub-bl@users.noreply.github.com>
2025-12-06 20:58:16 +0100

    Extend hashcode with a custom combine method that take infite arguments

---
+++ MossEngine.UI/Extensions/HashCodeExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace MossEngine.UI.Extensions;
+
+public static class HashCodeExtensions
+{
+	extension( HashCode )
+	{
+		public static HashCode Combine( params object[] values )
+		{
+			var hash = new HashCode();
+			
+			foreach ( var value in values )
+				hash.Add( value );
+			
+			return hash;
+		}
+	}
+}
